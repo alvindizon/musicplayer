5fc17fbc92b929fb2837f09a49960d3cf58702bf
Alvin Dizon <alvin.dizon91@gmail.com>
2017-10-31 10:52:50 +0100

    Added completion listener, removed buggy seekbar code
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.musicplayer;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer audioPlayer;
 
-    private Handler myHandler = new Handler();
-    private SeekBar seekBar;
+//    private Handler myHandler = new Handler();
+//    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +30,20 @@ public class MainActivity extends AppCompatActivity {
         Button pauseButton = (Button) findViewById(R.id.pause_button);
         Button stopButton = (Button) findViewById(R.id.stop_button);
 
-        seekBar = (SeekBar) findViewById(R.id.seek_bar);
-        seekBar.setClickable(false);
-
+//        seekBar = (SeekBar) findViewById(R.id.seek_bar);
+//        seekBar.setClickable(false);
+        audioPlayer = MediaPlayer.create(MainActivity.this, R.raw.number_five);
         // create listeners for buttons
         playButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // create audioPlayer object if there's none
-                if(audioPlayer == null){
-                    audioPlayer = MediaPlayer.create(MainActivity.this, R.raw.song);
-                }
+
                 // play the audio file
                 audioPlayer.start();
 
                 // get current position and duration
-                seekBar.setProgress(audioPlayer.getCurrentPosition());
-                myHandler.postDelayed(UpdateSongTime,100);
+//                seekBar.setProgress(audioPlayer.getCurrentPosition());
+//                myHandler.postDelayed(UpdateSongTime,100);
 
             }
         });
@@ -62,6 +61,13 @@ public class MainActivity extends AppCompatActivity {
                 stopPlayback();
             }
         });
+
+        audioPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(getApplicationContext(), "I'm done", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void stopPlayback(){
@@ -72,10 +78,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Runnable UpdateSongTime = new Runnable() {
-        public void run() {
-            seekBar.setProgress(audioPlayer.getCurrentPosition());
-            myHandler.postDelayed(this, 100);
-        }
-    };
+
+//    private Runnable UpdateSongTime = new Runnable() {
+//        public void run() {
+//            seekBar.setProgress(audioPlayer.getCurrentPosition());
+//            myHandler.postDelayed(this, 100);
+//        }
+//    };
 }
